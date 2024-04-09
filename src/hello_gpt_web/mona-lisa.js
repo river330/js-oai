36a3a3eed4778fd96e58ae3090c4027dc5e2379c
river330 <river330@newschool.edu>
2024-04-09 09:40:30 -0400

    added chalk

--- src/hello_gpt_web/mona-lisa.js
+++ src/hello_gpt_web/mona-lisa.js
@@ -3,6 +3,9 @@ import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { createExitSignal, staticServer } from "../shared/server.ts";
 import { gptPrompt, makeImage } from "../shared/openai.js";
 
+import { Chalk } from "npm:chalk@5";
+const chalk = new Chalk({ level: 1 });
+
 // change working dirctory to the current file's directory
 Deno.chdir(new URL(".", import.meta.url).pathname);
 // log the current working directory with friendly message
@@ -33,6 +36,8 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(staticServer);
 
-console.log("\nListening on http://localhost:8000");
+console.log(
+  "\nListening on " + chalk.blueBright.underline("http://localhost:8000"),
+);
 
 await app.listen({ port: 8000, signal: createExitSignal() });
